648bc53548095a44147eee5a11d85b2806102bf9
lucasafonso-solutis <lucas.afonso@solutis.com.br>
2026-08-28 08:55:56 -0300

    fix: admin credencials

--- GenHash.java
+++
@@ -1,6 +0,0 @@
-import org.springframework.security.crypto.bcrypt.BCrypt;
-public class GenHash {
-    public static void main(String[] args) {
-        System.out.println(BCrypt.hashpw("admin123", BCrypt.gensalt()));
-    }
-}
